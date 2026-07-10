7c7d8cc49d8675a8cfc816466c37cdf42d3df22a
bahhadirozen-DT <bahhadirozen@gmail.com>
2026-07-10 03:23:08 +0000

    feat: convert AuditEngine into complete audit pipeline

--- core/audit_engine.py
+++ core/audit_engine.py
@@ -1,6 +1,9 @@
 from core.loaders.document_loader import DocumentLoader
 from core.factory.parser_factory import ParserFactory
 
+from core.rules_engine import RulesEngine
+from core.similarity.similarity_engine import SimilarityEngine
+
 
 class AuditEngine:
 
@@ -9,8 +12,30 @@ class AuditEngine:
         self.loader = DocumentLoader()
         self.factory = ParserFactory()
 
+        self.rules = RulesEngine()
+        self.similarity = SimilarityEngine()
+
     def load_and_parse(self, filename):
 
         text = self.loader.load(filename)
 
         return self.factory.parse(text)
+
+    def audit(self, filename):
+
+        model = self.load_and_parse(filename)
+
+        rule_results = self.rules.audit(model)
+
+        similarity = {}
+
+        try:
+            similarity = self.similarity.compare(model)
+        except Exception:
+            similarity = {}
+
+        return {
+            "model": model,
+            "rules": rule_results,
+            "similarity": similarity,
+        }
